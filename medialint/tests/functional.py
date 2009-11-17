5e91b0e9fef6d88aeba7ff61d98256b2cd00368a
a3 <a3@a3-bmc.(none)>
2009-11-17 17:10:52 -0200

    migrate media directory to tests

--- medialint/tests/functional.py
+++ medialint/tests/functional.py
@@ -16,28 +16,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # Django settings for medialint_project project.
-from django.conf import settings
+from os.path import dirname, abspath, join
 from django.test import TestCase
 
 from medialint import CSSLint, InvalidCSSError
 from medialint.tests.utils import assert_raises
 
+LOCAL_FILE = lambda *x: join(abspath(dirname(__file__)), *x)
 class CSSLintFunctionalTest(TestCase):
     def test_can_find_css_files_for_given_path(self):
         'CSSLint.fetch_css should find css files within a given path'
-        css_files = CSSLint.fetch_css(settings.MEDIA_ROOT)
+        css_files = CSSLint.fetch_css(LOCAL_FILE('media'))
         self.assertTrue(isinstance(css_files, list))
         self.assertEquals(len(css_files), 2)
-        self.assertEquals(css_files[0], settings.LOCAL_FILE('media', 'css', 'invalid-css1.css'))
-        self.assertEquals(css_files[1], settings.LOCAL_FILE('media', 'css', 'valid', 'valid-css1.css'))
+        self.assertEquals(css_files[0], LOCAL_FILE('media', 'css', 'invalid-css1.css'))
+        self.assertEquals(css_files[1], LOCAL_FILE('media', 'css', 'valid', 'valid-css1.css'))
 
     def test_find_and_check_css_error(self):
         'CSSLint.fetch_and_check should find and check css files'
-        fname = settings.LOCAL_FILE('media', 'css', 'invalid-css1.css')
+        fname = LOCAL_FILE('media', 'css', 'invalid-css1.css')
         assert_raises(InvalidCSSError,
-                      CSSLint.check_files, settings.MEDIA_ROOT,
+                      CSSLint.check_files, LOCAL_FILE('media'),
                       exc_pattern=r'Syntax error on file: %s line 4 column 11. Got the unexpected char ":"' % fname)
 
     def test_find_and_check_css_success(self):
         'CSSLint.fetch_and_check should find and check css files'
-        assert CSSLint.check_files(settings.LOCAL_FILE('media', 'css', 'valid')) is True, 'Should check successfully'
+        assert CSSLint.check_files(LOCAL_FILE('media','css', 'valid')) is True, 'Should check successfully'
