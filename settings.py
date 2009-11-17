03efeab4a843333051ea4d786282041eadc3715a
Gabriel Falcão <gabriel@nacaolivre.org>
2009-11-17 14:18:15 -0200

    Starting the project

---
+++ settings.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# <Django Media Lint - CSS and JS lint checker for django>
+# Copyright (C) <2009>  Gabriel Falcão <gabriel@nacaolivre.org>
+# Copyright (C) <2009>  Benitez Moretti Coelho <benitezmc@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Django settings for medialint_project project.
+
+from os.path import dirname, abspath, join
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    (u'Gabriel Falcão', 'gabriel@nacaolivre.org'),
+    (u'Benitez Moretti Coelho', 'benitezmc@gmail.com'),
+)
+
+MANAGERS = ADMINS
+LOCAL_FILE = lambda *x: join(abspath(dirname(__file__)), *x)
+
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = LOCAL_FILE('dummy.db')             # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+TIME_ZONE = 'America/Chicago'
+
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+MEDIA_ROOT = LOCAL_FILE('media')
+
+MEDIA_URL = ''
+
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'tmpx_#f&$&ss0oa)a9pg-lm-b1s^r+-8kjv^w%k#xc6)82fye1'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'medialint_project.urls'
+
+TEMPLATE_DIRS = (
+    LOCAL_FILE('templates'),
+)
+
+INSTALLED_APPS = (
+    'medialint',
+)
