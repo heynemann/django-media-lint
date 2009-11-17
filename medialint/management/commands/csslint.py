56aff02f6443f83ba1471670df9e1b50e8f6925f
a3 <a3@a3-bmc.(none)>
2009-11-17 17:29:43 -0200

    command to call csslint

---
+++ medialint/management/commands/csslint.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# <Django Media Lint - CSS and JS lint checker for django>
+# Copyright (C) <2009>  Gabriel Falcão <gabriel@nacaolivre.org>
+# Copyright (C) <2009>  Benitez Moretti Coelho <benitezmc@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Django settings for medialint_project project.
+
+
+from django.core.management.base import BaseCommand
+from optparse import make_option
+from medialint import CSSLint
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--path', dest='path', default=None,
+            help = "Validate css files"),
+        
+    )
+
+    def handle(self, *args, **options):
+        CSSLint.check_files(options.get('path'))
+
